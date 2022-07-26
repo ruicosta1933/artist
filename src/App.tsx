769eec902f7045766bc50630ce0811cfa5401fd5
ruicosta1933 <ruicosta607@gmail.com>
2022-07-26 14:57:54 +0100

    Events

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import styles from "./App.module.css";
 import { Banner } from './components/banner/banner';
 import { Music } from './components/music';
 import { Footer } from './components/footer/footer';
+import { Events } from './components/events/events';
 function App() {
   return (
     <>
     <Header />
     <Banner />
     <Music />
+    <Events />
     <Footer />
     </>
     
---
+++ src/components/events/events.tsx
@@ -0,0 +1,19 @@
+import styles from "./events.module.css";
+
+export function Events() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.divImage}>
+                {/* <div id={styles.showImage}></div> */}
+              <img className={styles.image} src="cartaz.jpeg"/>
+            </div>
+            <div className={styles.divText} >
+                <a>
+                <h1>La Cantera | <a style={{textDecoration:"none"}} href="https://www.instagram.com/smokingbars.oficial/" target="_blank">Smoking Bars</a></h1>
+             
+                <h3 className={styles.text}> 6 de Agoste | Lisboa</h3>
+                </a>
+            </div>
+        </div>
+    )
+}
