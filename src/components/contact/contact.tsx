da5f2bdb6d006424502017a93237a436635da632
unknown <ruicosta607@gmail.com>
2022-08-03 14:36:35 +0100

    changes

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -2,17 +2,19 @@ import styles from "./contact.module.css";
 import { AiOutlineInstagram } from "react-icons/ai"
 import { SiGmail } from "react-icons/si"
 import { FiTwitter } from "react-icons/fi"
+import { FaTiktok } from "react-icons/fa"
+
 export default function Contact() {
     return (
                 <div  className={styles.container}>
                     <div className={styles.group}>
-                       <AiOutlineInstagram size={40}/> <a target="_blank" href="https://www.instagram.com/rui_costa_01/"><b>daInstagram</b> </a>
+                       <AiOutlineInstagram size={40}/> <a target="_blank" href="https://www.instagram.com/iamdacosta_/"><b>daInstagram</b> </a>
                     </div>
                     <div className={styles.group}>
                        <SiGmail size={40}/> <a target="_blank" href="mailto:ruicosta607@gmail.com"> <b>daEmail</b></a>
                     </div>
                     <div className={styles.group}>
-                       <FiTwitter size={40} /> <a target="_blank" href="https://twitter.com/ruicosta607"> <b>daTwitter</b></a>
+                       <FaTiktok size={40} /> <a target="_blank" href="https://www.tiktok.com/@iamdacosta_"><b>daTikTok</b></a>
                     </div>
                 </div>
         )
--- src/components/events/events.tsx
+++ src/components/events/events.tsx
@@ -9,10 +9,15 @@ export function Events() {
             </div>
             <div className={styles.divText} >
                 <a>
-                <h1>La Cantera | <a style={{textDecoration:"none"}} href="https://www.instagram.com/smokingbars.oficial/" target="_blank">Smoking Bars</a></h1>
+                <h1 className={styles.text}>La Cantera |<a style={{textDecoration:"none"}} href="https://www.instagram.com/smokingbars.oficial/" target="_blank"> &nbsp;Smoking Bars</a></h1>
              
                 <h3 className={styles.text}> 6 de Agosto | Lisboa</h3>
+              
+                <p className={styles.text}>La Cantera é um evento de Batalhas de RAP organizado pela Smoking Bars.</p>
+                <p className={styles.text}>Este evento tem o intuito de dar palco a rappers, com menores dimensões, de se poderem mostrar e provar o seu valor !</p>
+            
                 </a>
+               
             </div>
         </div>
     )
