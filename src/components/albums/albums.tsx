567049a4c7f73db6602ede15e7c4e50735763fff
unknown <ruicosta607@gmail.com>
2022-08-05 18:42:25 +0100

    bugs fixed

--- src/components/albums/albums.tsx
+++ src/components/albums/albums.tsx
@@ -7,7 +7,6 @@ export default function Albums() {
             <div className={styles.division}>
                 <div className={styles.imageDiv} style={{marginRight:"10px"}}>
                     <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/album/0Iafs1Is7AICsW4IF1kQ26?utm_source=generator" allowFullScreen width="100%" height="100%" frameBorder="0"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-                    
                 </div>
                 <div className={styles.desc}> 
                     <h2><a target="_blank" href='https://open.spotify.com/album/0Iafs1Is7AICsW4IF1kQ26?si=rGhk-GlwQfKJD8MDrGQDiw'>Não Gosto Nem Desgosto (Muito pelo Contrário)</a></h2>
@@ -37,7 +36,7 @@ export default function Albums() {
                     <p>&emsp;&emsp;&#9679; <a target="_blank" href="https://soundcloud.com/iamdacosta/slim-mathers-x-justino-vista-1">Slim Mathers X Justino - Vista</a></p>
                 </div>
                 <div className={styles.imageDiv} style={{marginLeft:"10px"}}>
-                <iframe width="100%" height="100%" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1355541847&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div className={styles.soundCloud}><a href="https://soundcloud.com/iamdacosta" title="dacosta" target="_blank" className={styles.soundCloud2}>dacosta</a> · <a href="https://soundcloud.com/iamdacosta/sets/intelligence-with-wings-trailer" title="Intelligence with Wings (Trailer)" target="_blank" className={styles.soundCloud2}>Intelligence with Wings (Trailer)</a></div>
+                <iframe width="100%" height="450" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1355541847&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div className={styles.soundCloud}><a href="https://soundcloud.com/iamdacosta" title="dacosta" target="_blank" className={styles.soundCloud2}>dacosta</a> · <a href="https://soundcloud.com/iamdacosta/sets/intelligence-with-wings-trailer" title="Intelligence with Wings (Trailer)" target="_blank" className={styles.soundCloud2}>Intelligence with Wings (Trailer)</a></div>
                 </div>
             </div>
         </div>
