ce3144c3e960713ba695e619e1a6e8f6d0f741eb
unknown <ruicosta607@gmail.com>
2022-08-11 19:00:57 +0100

    another

--- src/components/header/logo/logo.tsx
+++ src/components/header/logo/logo.tsx
@@ -3,7 +3,7 @@ import styles from './logo.module.css';
 export function Logo() {
     return (
         <div className={styles.container}>
-           <a href="/"><img  className={styles.logo} src="https://see.fontimg.com/api/renderfont4/vmgK9/eyJyIjoiZnMiLCJoIjo1MCwidyI6MTAwMCwiZnMiOjUwLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/UnVpIENvc3Rh/billion-dreams.png" alt="Cursive fonts"/></a>
+             <a href='/'><img className={styles.logo} src='/Rui.png'/></a>
         </div>
     )
 }
