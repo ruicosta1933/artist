52eb47c7462440e602a67a1545d24826265de1b7
unknown <ruicosta607@gmail.com>
2022-08-22 04:00:46 +0100

    logo

--- src/components/header/logo/logo.tsx
+++ src/components/header/logo/logo.tsx
@@ -3,7 +3,7 @@ import styles from './logo.module.css';
 export function Logo() {
     return (
         <div className={styles.container}>
-             <a href='/'><img className={styles.logo} src='/Rui.png'/></a>
+             <a href='/'><img className={styles.logo} src='/logo.png'/></a>
         </div>
     )
 }
